cda411f26e7e04dc0939e14641fdd382ae948721
shakhawat <md.shakhawat@bjitgroup.com>
2024-01-12 15:58:39 +0600

    replace  banner image with group image

--- src/components/BannerImage/index.tsx
+++ src/components/BannerImage/index.tsx
@@ -1,37 +1,20 @@
 import React from "react";
 import "./index.scss";
-import user from "../../../public/assets/images/human/image1.png";
-import shape from "../../../public/assets/images/shape/shape2.png";
-import stat from "../../../public/assets/images/stat/stat1.png";
+import user from "../../../public/assets/images/group/image-1.png";
+
 import Image from "next/image";
 type Props = {};
 
 export default function BannerImage({}: Props) {
   return (
     <div className="banner__image">
-      <div className="banner__image-box">
-        <Image
-          src={user.src}
-          height={608}
-          width={590}
-          alt="user image"
-          className="banner__image-picture"
-        />
-        <Image
-          src={shape.src}
-          height={136}
-          width={131}
-          alt="arrow image"
-          className="banner__image-arrow"
-        />
-        <Image
-          src={stat.src}
-          height={167}
-          width={193}
-          alt="arrow image"
-          className="banner__image-stat"
-        />
-      </div>
+      <Image
+        src={user.src}
+        width={787}
+        height={608}
+        alt="user image"
+        className="banner__image__picture"
+      />
     </div>
   );
 }
--- src/components/ContactFormImage/index.tsx
+++ src/components/ContactFormImage/index.tsx
@@ -1,46 +1,20 @@
 import Image from "next/image";
 import React from "react";
 import "./index.scss";
-import user from "../../../public/assets/images/human/image3.png";
-import line from "../../../public/assets/images/shape/shape1.png";
-import fire from "../../../public/assets/images/shape/fire.png";
-import stat from "../../../public/assets/images/stat/stat2.png";
+import user from "../../../public/assets/images/group/image-3.png";
 
 type Props = {};
 
 export default function ContactFormImage({}: Props) {
   return (
     <div className="contact-form-image">
-      <div className="contact-form-image__box">
-        <Image
-          src={user.src}
-          width={379}
-          height={553}
-          alt="user image"
-          className="contact-form-image__picture"
-        />
-        <Image
-          src={line.src}
-          height={97.53}
-          width={155.8}
-          alt="line image"
-          className="contact-form-image__line"
-        />
-        <Image
-          src={fire.src}
-          height={79.3}
-          width={62.8}
-          alt="shape image"
-          className="contact-form-image__fire"
-        />
-        <Image
-          src={stat.src}
-          width={267}
-          height={216}
-          alt="statistic"
-          className="contact-form-image__stat"
-        />
-      </div>
+      <Image
+        src={user.src}
+        width={679}
+        height={553}
+        alt="user image"
+        className="contact-form-image__picture"
+      />
     </div>
   );
 }
